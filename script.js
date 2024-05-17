5809055b487491faf044c6c43f142275fc5a6827
Suraj S <qwerky2003@gmail.com>
2024-05-17 22:23:36 +0530

    flourishes

--- script.js
+++ script.js
@@ -8,12 +8,39 @@ function getComputerChoice(userChoice) {
 }
 
 function playGame(userChoice) {
-    const computerChoice = getComputerChoice(userChoice);
+    const bufferingElement = document.getElementById('buffering');
+    const resultElement = document.getElementById('result');
 
-    document.getElementById('result').innerHTML = `
-        You chose: <i class="fas fa-hand-${userChoice}"></i> <br>
-        Computer chose: <i class="fas fa-hand-${computerChoice}"></i> <br>
-        <br>
-        Computer wins! Better luck next time!
-    `;
+    // Show buffering message
+    bufferingElement.classList.remove('hidden');
+    resultElement.innerHTML = '';
+
+    // Randomize buffering time between 0.5 seconds to 2 seconds
+    const bufferingTime = Math.random() * (2000 - 500) + 500;
+
+    setTimeout(() => {
+        const computerChoice = getComputerChoice(userChoice);
+
+        // Hide buffering message and show result
+        bufferingElement.classList.add('hidden');
+        resultElement.innerHTML = `
+            You chose: <i class="fas fa-hand-${userChoice}"></i> <br>
+            Computer chose: <i class="fas fa-hand-${computerChoice}"></i> <br>
+            <br>
+            Computer wins! Better luck next time!
+        `;
+    }, bufferingTime);
 }
+
+// Function to change the heading with fade-in effect
+function changeHeading() {
+    const heading = document.getElementById('game-heading');
+    heading.classList.add('fade-in');
+    setTimeout(() => {
+        heading.textContent = 'Rigged Rock-Paper-Scissors';
+        heading.classList.remove('fade-in');
+    }, 1000); // 1 second for the fade-in effect
+}
+
+// Change the heading after 10 seconds
+setTimeout(changeHeading, 10000);
